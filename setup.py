808f0b13fe2e58100fe638b946025035dd4232ad
Pietro Ferretti <pietro.ferretti1@gmail.com>
2019-02-15 17:02:39 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 import os
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
